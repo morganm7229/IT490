e3b91a216294f485789413dd74a40b4b72f8a956
morganm7229 <rm833@njit.edu>
2022-10-26 16:32:26 -0400

    pasting everything to the database folder

--- web/database/testRabbitMQClient.php
+++ web/database/testRabbitMQClient.php
@@ -5,14 +5,20 @@ require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
 $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
-
+if (isset($argv[1]))
+{
+  $msg = $argv[1];
+}
+else
+{
+  $msg = "test message";
+}
 
 $request = array();
-$request['type'] = "login";
-$request['username'] = "josh";
-$request['password'] = "54321";
-$request['message'] = "HI";
-echo "Sending request";
+$request['type'] = "Login";
+$request['username'] = "steve";
+$request['password'] = "password";
+$request['message'] = $msg;
 $response = $client->send_request($request);
 //$response = $client->publish($request);
 
@@ -21,4 +27,4 @@ print_r($response);
 echo "\n\n";
 
 echo $argv[0]." END".PHP_EOL;
-//test
+
